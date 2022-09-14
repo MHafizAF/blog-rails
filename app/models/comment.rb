4b2bb4cc2103e4e32f64d1b0006d5135d7d0f945
Muhammad Hafiz Al Fikri <hafiz170301@gmail.com>
2022-09-14 09:30:17 +0700

    adding new validation for association in post and comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,6 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
+  validates_associated :user
   validates :comment, presence: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,6 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   belongs_to :user
 
+  validates_associated :user
   validates :title, :body, presence: true
 end
